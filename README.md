2fb663fc225d9402947da659554b096fdc7c1278
BitBone <bitbone123@gmail.com>
2020-02-25 17:09:40 -0800

    no message

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectBF
+
+Hi team!
---
+++ projectBF/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+    private float moveInput;
+    public float sprintSpeed;
+    private float sprint;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    private InputManager inputManager;
+
+    void Start()
+    {
+        inputManager = GameObject.FindObjectOfType<InputManager>();
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        if (inputManager.GetButton("Sprint"))
+        {
+            sprint = sprintSpeed;
+        }
+        else
+        {
+            sprint = 1;
+        }
+
+        moveInput = 0;
+
+        if (inputManager.GetButton("MoveLeft"))
+        {
+            moveInput = -1;
+        }
+
+         if (inputManager.GetButton("MoveRight"))
+        {
+            moveInput = 1;
+        }
+
+        rb.velocity = new Vector2(moveInput * moveSpeed * sprint, rb.velocity.y);
+
+    }
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if (inputManager.GetButtonDown("Jump") && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+
+        }
+        else if (inputManager.GetButtonDown("Jump") && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+
+        }
+    }
+}
+
+//        moveInput = Input.GetAxisRaw("Horizontal");
